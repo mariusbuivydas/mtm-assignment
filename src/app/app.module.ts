14d9b3c4bffa102e99cc4d22b729cb58530bdc05
Marius Buivydas <marius.buivydas91@gmail.com>
2019-08-05 01:57:19 +0300

    Added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { RoutingModule } from './routing/routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PersonaModule } from '@components/persona/persona.module';
+import { DashboardComponent } from '@components/dashboard/dashboard.component';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     NavbarComponent,
   ],
   imports: [
---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  styleUrls: ['./dashboard.component.scss'],
+  templateUrl: './dashboard.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class DashboardComponent {}
